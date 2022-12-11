cac0bd9c0cb4d5ff38507bebcacaacea83982a4f
Moojun <mgim32137@gmail.com>
2022-12-11 10:31:47 +0900

    74 - getPrevNotice 메소드의 SQL 쿼리 작성하기

--- src/main/java/com/example/jsp_practice/Service/NoticeService.java
+++ src/main/java/com/example/jsp_practice/Service/NoticeService.java
@@ -12,12 +12,11 @@ public class NoticeService {
     }
 
     public List<Notice> getNoticeList(int page) {
-        String sql = "SELECT * FROM (" +
-                " SELECT @ROWNUM := @ROWNUM + 1 AS ROWNUM, N.*" +
-                " FROM (SELECT * FROM board ORDER BY regdate DESC) AS N" +
-                ") AS A" +
-                " WHERE (ROWNUM BETWEEN 6 AND 10)" +
-                " AND  (@ROWNUM := 0) = 0";
+        String sql = "SELECT * FROM ( " +
+                "SELECT @ROWNUM := @ROWNUM + 1 AS ROWNUM, N.* " +
+                "FROM (SELECT * FROM notice ORDER BY REGDATE ASC) AS N " +
+                ") AS A, (SELECT @ROWNUM := 0) AS B " +
+                "WHERE (ROWNUM BETWEEN 6 AND 10)";
 
         return getNoticeList("title", "", page);
     }
@@ -61,6 +60,13 @@ public class NoticeService {
     }
 
     public Notice getPrevNotice(int id) {
+        String sql = "SELECT @ROWNUM := @ROWNUM + 1 AS ROWNUM, A.* " +
+                "FROM (SELECT * FROM notice ORDER BY regdate DESC) AS A, " +
+                "(SELECT @ROWNUM := 0) AS R " +
+                "WHERE regdate <  ( " +
+                "SELECT regdate FROM notice WHERE id = 3 " +
+                ") " +
+                "LIMIT 1 ";
 
         return null;
     }
